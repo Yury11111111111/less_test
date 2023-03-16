44fc68e34356ecd78a82164fe7e880efcd861d6c
unknown <yury.ovchinnkov@yandex.ru>
2023-03-16 19:43:25 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./style/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>Less test</header>
+      <div className="main">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui ipsam
+        impedit repellendus eligendi ad vero laboriosam minima temporibus, aut
+        exercitationem totam ipsum voluptatem eum, aspernatur odio possimus
+        blanditiis nihil saepe!
+      </div>
+    </div>
+  );
+}
+
+export default App;
